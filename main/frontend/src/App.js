8c5aa4d06b639822b763e609326f3a30f2ac330d
hault033092 <letrunghau090298@gmail.com>
2022-07-18 10:11:03 +0700

    initialize the project structure

---
+++ main/frontend/src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello world</h1>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
