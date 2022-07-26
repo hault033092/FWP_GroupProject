acd972edd5cfd22e2f9d8a0bc339ede3c74c6479
hault033092 <letrunghau090298@gmail.com>
2022-07-26 13:08:09 +0700

    make modifications

---
+++ beelancer_main/frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div></div>
+  )
+
+
+}
+
+export default Header
