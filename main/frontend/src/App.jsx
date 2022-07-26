cc7dd4519ca31b433a57d7ce37472141698958be
KultrasHub <68211943+KultrasHub@users.noreply.github.com>
2022-07-26 13:40:00 +0700

    testing code

---
+++ main/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import FooterSection from './FooterSection'
+
+const App=() => {
+  return (
+    <div className='App'>
+      <FooterSection />
+    </div>
+  )
+}
+
+export default App
---
+++ main/frontend/src/FooterSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './FooterSection.css'
+export default function FooterSection() {
+  return (
+    <div className='FooterSection'>
+    <p>Footer</p>
+    </div>
+  )
+}
