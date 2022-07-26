97f3776d11f872c7819a4cf9e9971d46c0cef162
hault033092 <letrunghau090298@gmail.com>
2022-07-26 13:31:50 +0700

    add code structure for some pages

---
+++ beelancer_main/frontend/src/pages/BeFreelancer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BeFreelancer = () => {
+  return (
+    <div>BeFreelancer</div>
+  )
+}
+
+export default BeFreelancer
---
+++ beelancer_main/frontend/src/pages/FindFreelancer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FindFreelancer = () => {
+  return (
+    <div>FindFreelancer</div>
+  )
+}
+
+export default FindFreelancer
