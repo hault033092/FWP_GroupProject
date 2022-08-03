a664ae97bc24565cab2825538a4442b837c44406
hault033092 <letrunghau090298@gmail.com>
2022-08-03 22:21:50 +0700

    add JobPost

--- beelancer_main/frontend/src/pages/Pages.jsx
+++ beelancer_main/frontend/src/pages/Pages.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import AboutUs from './AboutUs'
 import BeFreelancer from './BeFreelancer'
 import FindFreelancer from './FindFreelancer'
 import Home from './Home'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
-
+import PostJob from './PostJob'
 
 const Pages = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/AboutUs' element={<AboutUs/> }/>
-      <Route path='/SignIn' element={<SignIn/>}/>
-      <Route path='/SignUp' element={<SignUp/>}/>
-      <Route path='/FindFreelancer' element={<FindFreelancer/>}/>
-      <Route path='/BeFreelancer' element={<BeFreelancer/>}/>
+      <Route path='/' element={<Home />} />
+      <Route path='/AboutUs' element={<AboutUs />} />
+      <Route path='/SignIn' element={<SignIn />} />
+      <Route path='/SignUp' element={<SignUp />} />
+      <Route path='/FindFreelancer' element={<FindFreelancer />} />
+      <Route path='/BeFreelancer' element={<BeFreelancer />} />
+      <Route path='/PostJob' element={<PostJob />} />
     </Routes>
   )
 }
