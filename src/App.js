133c6c2a370221719f58812e9b76107f78357b7f
hault033092 <letrunghau090298@gmail.com>
2022-08-23 15:35:44 +0700

    delete redundant files

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import RouterView from "./components/common/RouterView";
-import "./App.css";
-
-const App = () => {
-	return <RouterView />;
-};
-
-export default App;
--- src/components/styles/Global.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const DivFlexComponent = styled.div`
-	display: flex;
-	justify-content: ${({ justify }) => justify || "flex-start"};
-	align-items: ${({ align }) => align || "flex-start"};
-`;
